7580481f846ba1114f953529dad06fa9e74cb91f
root <root@vultr.guest>
2016-11-29 11:48:42 +0800

    pracetice

---
+++ indexx.php
@@ -0,0 +1,9 @@
+<?php
+//$link=mysqli_connect("localhost","root","4892993abc1234","woden");
+//mysqli_set_charset($link,"utf8");
+//var_dump($link);die;
+ require("db.class.php");
+ $data['name']="zhanmuqiang";
+//$result= db::insert('note',$data);
+phpinfo();
+//var_dump($result);die;
